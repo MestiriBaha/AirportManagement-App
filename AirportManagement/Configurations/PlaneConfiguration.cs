ec8a7b30633c1273f2a595ba2a5bc3be135e091a
baha mestiri <mohamedbaha.mestiri@gmail.com>
2022-11-16 21:48:10 +0100

    Configuring the Fluent Api Part

--- AirportManagement/Configurations/PlaneConfiguration.cs
+++ AirportManagement/Configurations/PlaneConfiguration.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Design ;
+
 
 namespace AirportManagement.Configurations
 {
     public  class PlaneConfiguration
     {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
